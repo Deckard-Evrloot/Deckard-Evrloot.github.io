4c70c51150662d9745c5fbd1285b4d784a87134c
Deckard-Evrloot <116961236+Deckard-Evrloot@users.noreply.github.com>
2023-04-02 21:15:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const futureEventsContainer = document.querySelector('.future-events');
   const pastEventsContainer = document.querySelector('.past-events');
 
-  const API_KEY = 'AIzaSyAxSUaEX5hAPxNC8SLBSht1OP23wnQJwBo';
+  const API_KEY = 'AIzaSyCiIHjuNf7gMLvBDk6IN9MGC5zQgpsoEas';
   const SPREADSHEET_ID = '1Y7-F9gCfa16R73lnh_ALXdLTLr9OZmSrnIqkx0g2TtQ';
   const SHEET_NAME = 'EventDatabaseEvrloot';
   const FALLBACK_IMAGE_URL = 'https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg';
