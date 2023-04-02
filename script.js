0a0fcc7b2653b8672f7ae21b822cf9afe19d53c0
Deckard-Evrloot <116961236+Deckard-Evrloot@users.noreply.github.com>
2023-04-02 18:20:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ document.addEventListener('DOMContentLoaded', async () => {
   const futureEventsContainer = document.querySelector('.future-events');
   const pastEventsContainer = document.querySelector('.past-events');
 
+  const API_KEY = 'YOUR_ACTUAL_API_KEY';
+  const SPREADSHEET_ID = 'YOUR_SPREADSHEET_ID';
+  const SHEET_NAME = 'Sheet1';
+
   async function fetchEventData() {
     try {
-      const response = await fetch('https://sheet.best/api/sheets/1e3ac16b-2b20-4c0f-a327-d10f43f42eae');
+      const url = `https://sheets.googleapis.com/v4/spreadsheets/${1Y7-F9gCfa16R73lnh_ALXdLTLr9OZmSrnIqkx0g2TtQ}/values/${EventDatabaseEvrloot}?key=${AIzaSyAxSUaEX5hAPxNC8SLBSht1OP23wnQJwBo}`;
+      const response = await fetch(url);
       const data = await response.json();
-      return data;
+      const headers = data.values[0];
+      const rows = data.values.slice(1);
+      const formattedData = rows.map((row) => {
+        const obj = {};
+        headers.forEach((header, index) => {
+          obj[header] = row[index];
+        });
+        return obj;
+      });
+
+      return formattedData;
     } catch (error) {
       console.error('Error fetching event data:', error);
     }
@@ -45,4 +60,44 @@ document.addEventListener('DOMContentLoaded', async () => {
     eventTitle.textContent = eventData.Title;
 
     const eventLocation = document.createElement('p');
-    eventLocation
+    eventLocation.textContent = `Location: ${eventData.Location}`;
+
+    eventDetails.appendChild(eventTitle);
+    eventDetails.appendChild(eventLocation);
+
+    eventDiv.appendChild(eventImageContainer);
+    eventDiv.appendChild(eventTime);
+    eventDiv.appendChild(eventDetails);
+
+    return eventDiv;
+  }
+
+  function compareDates(a, b) {
+    const dateA = new Date(a.Time);
+    const dateB = new Date(b.Time);
+    return dateA - dateB;
+  }
+
+  function isPastEvent(eventData) {
+    const eventDate = new Date(eventData.Time);
+    const currentDate = new Date();
+    return eventDate < currentDate;
+  }
+
+  async function displayEvents() {
+    const eventsData = await fetchEventData();
+    const sortedEventsData = eventsData.sort(compareDates);
+
+    sortedEventsData.forEach((eventData) => {
+      const eventElement = createEventElement(eventData);
+
+      if (isPastEvent(eventData)) {
+        pastEventsContainer.appendChild(eventElement);
+      } else {
+        futureEventsContainer.appendChild(eventElement);
+      }
+    });
+  }
+
+  displayEvents();
+});
