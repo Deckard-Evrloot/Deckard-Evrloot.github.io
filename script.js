24c517a43e5112b8bd1749f47f369cf3b7f762df
Deckard-Evrloot <116961236+Deckard-Evrloot@users.noreply.github.com>
2023-04-08 12:26:34 +0200

    Changed createEventElement

--- script.js
+++ script.js
@@ -46,50 +46,52 @@ document.addEventListener('DOMContentLoaded', async () => {
       console.error('Error fetching event data:', error);
     }
   }
+  
   function createEventElement(eventData) {
-    const eventDiv = document.createElement('div');
-    eventDiv.classList.add('event');
+  const eventDiv = document.createElement('div');
+  eventDiv.classList.add('event');
 
-    const imageContainer = document.createElement('div');
-    imageContainer.classList.add('image-container');
-    const eventImage = document.createElement('img');
-    eventImage.src = eventData.ImageURL !== '' ? eventData.ImageURL : FALLBACK_IMAGE_URL;
-    imageContainer.appendChild(eventImage);
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('image-container');
+  const eventImage = document.createElement('img');
+  eventImage.src = eventData.ImageURL !== '' ? eventData.ImageURL : FALLBACK_IMAGE_URL;
+  imageContainer.appendChild(eventImage);
 
-    const eventTime = document.createElement('div');
-    eventTime.classList.add('event-time');
-    const eventDate = eventData.Time;
-    const formattedDate = formatDate(eventDate);
-    eventTime.textContent = formattedDate;
-    imageContainer.appendChild(eventTime);
+  const eventTime = document.createElement('div');
+  eventTime.classList.add('event-time');
+  const eventDate = eventData.Time;
+  const formattedDate = formatDate(eventDate);
+  eventTime.textContent = formattedDate;
+  imageContainer.appendChild(eventTime);
 
-    const freeSlots = document.createElement('div');
-    freeSlots.classList.add('free-slots');
-    freeSlots.textContent = `Freie Plätze: ${eventData['Free Slots']}`;
-    imageContainer.appendChild(freeSlots);
+  const freeSlots = document.createElement('div');
+  freeSlots.classList.add('free-slots');
+  freeSlots.textContent = `Freie Plätze: ${eventData['Free Slots']}`;
+  imageContainer.appendChild(freeSlots);
 
-    const detailsDiv = document.createElement('div');
-    detailsDiv.classList.add('details');
+  const detailsDiv = document.createElement('div');
+  detailsDiv.classList.add('details');
 
-    const title = document.createElement('h1');
-    title.textContent = eventData.Title;
-    detailsDiv.appendChild(title);
+  const title = document.createElement('h1');
+  title.textContent = eventData.Title;
+  detailsDiv.appendChild(title);
 
-    const location = document.createElement('p');
-    location.textContent = `Ort: ${eventData.Location}`;
-    detailsDiv.appendChild(location);
+  const location = document.createElement('p');
+  location.textContent = `Ort: ${eventData.Location}`;
+  detailsDiv.appendChild(location);
 
-    const signUpButton = document.createElement('a');
-    signUpButton.href = `mailto:team@evrloot.io?subject=Anmeldung für Event: ${eventData.Title}`;
-    signUpButton.classList.add('sign-up-button');
-    signUpButton.innerHTML = 'Anmelden 📧';
-    detailsDiv.appendChild(signUpButton);
+  const signUpButton = document.createElement('a');
+  signUpButton.href = `mailto:team@evrloot.io?subject=Anmeldung für Event: ${eventData.Title}`;
+  signUpButton.classList.add('sign-up-button');
+  signUpButton.innerHTML = 'Anmelden 📧';
+  detailsDiv.appendChild(signUpButton);
 
-    eventDiv.appendChild(imageContainer);
-    eventDiv.appendChild(detailsDiv);
+  eventDiv.appendChild(imageContainer);
+  eventDiv.appendChild(detailsDiv);
 
-    return eventDiv;
-  }
+  return eventDiv;
+}
+
 
   
  function displayNoUpcomingEventsMessage() {
